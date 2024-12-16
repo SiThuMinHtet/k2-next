3700e3154685b8c965c611a16729be37eec033f5
sithu <sithuminhtet67@gmail.com>
2024-12-16 11:30:41 +0630

    sithu

--- components/LoginForm.js
+++ components/LoginForm.js
@@ -1,38 +1,37 @@
-import { useLogin } from '@/contexts/LoginContext'
-import Image from 'next/image'
-import styles from '@/styles/LoginForm.module.scss'
-import InputField from './InputField.js'
+import Image from "next/image";
+import styles from "@/styles/LoginForm.module.scss";
+import InputField from "./InputField.js";
 export default function LoginForm() {
   // const { login } = useLogin()
 
   const handleSubmit = async (e) => {
-    e.preventDefault()
-    const formData = new FormData(e.target)
-    await login(formData.get('email'), formData.get('password'))
-  }
+    e.preventDefault();
+    const formData = new FormData(e.target);
+    await login(formData.get("email"), formData.get("password"));
+  };
 
   return (
     <div className={styles.loginForm}>
       <div className={styles.loginFormBox}>
-        <Image 
-          src="/images/login/logo.png" 
-          alt="K2 Logo" 
-          width={80} 
-          height={70} 
+        <Image
+          src="/images/login/logo.png"
+          alt="K2 Logo"
+          width={80}
+          height={70}
           className={styles.mainLogo}
         />
         <form onSubmit={handleSubmit} className={styles.loginFormBox}>
-        <InputField 
-            type="email" 
-            name="email" 
-            placeholder="Email" 
-            icon="fa-envelope" 
+          <InputField
+            type="email"
+            name="email"
+            placeholder="Email"
+            icon="fa-envelope"
           />
-          <InputField 
-            type="password" 
-            name="password" 
-            placeholder="Password" 
-            icon="fa-lock" 
+          <InputField
+            type="password"
+            name="password"
+            placeholder="Password"
+            icon="fa-lock"
           />
 
           <button type="submit" className={styles.loginBtn}>
@@ -41,6 +40,5 @@ export default function LoginForm() {
         </form>
       </div>
     </div>
-  )
+  );
 }
-
--- pages/index.js
+++ pages/index.js
@@ -1,26 +1,24 @@
 import MainLayout from "@/components/MainLayout";
 import LoginForm from "@/components/LoginForm";
-import { LoginProvider } from "@/contexts/LoginContext";
+// import { LoginProvider } from "@/contexts/LoginContext.js";
 import styles from "@/styles/Home.module.scss";
 
 export default function Home() {
   return (
-    <LoginProvider>
-      <MainLayout>
-        <div className={styles.loginContainer}>
-          <div className={styles.loginPage}>
-            <div className={styles.loginPageFlex}>
-              <div className={styles.loginPageText}>
-                <h1>建工管理システム</h1>
-                <h1 className={styles.chgColor}>管理パネル</h1>
-                <p>For Web Applications, CRM, SFS, Admin Panels and more.</p>
-              </div>
+    <MainLayout>
+      <div className={styles.loginContainer}>
+        <div className={styles.loginPage}>
+          <div className={styles.loginPageFlex}>
+            <div className={styles.loginPageText}>
+              <h1>建工管理システム</h1>
+              <h1 className={styles.chgColor}>管理パネル</h1>
+              <p>For Web Applications, CRM, SFS, Admin Panels and more.</p>
             </div>
           </div>
-          <div className={styles.box}></div>
-          <LoginForm />
         </div>
-      </MainLayout>
-    </LoginProvider>
+        <div className={styles.box}></div>
+        <LoginForm />
+      </div>
+    </MainLayout>
   );
 }
