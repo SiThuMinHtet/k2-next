d1d7b435f11f3a314a321769ccd3f5325468a3dd
yy-mizu <lynnx101@icloud.com>
2024-12-16 08:29:10 +0630

    lynn16.10.24_2

--- components/MainLayout.js
+++ components/MainLayout.js
@@ -1,7 +1,6 @@
 import Head from 'next/head'
 
 
-
 export default function MainLayout({ children, title = '建工管理システム' }) {
   return (
     <div>
@@ -9,17 +8,13 @@ export default function MainLayout({ children, title = '建工管理システム
         <title>{title}</title>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.1/css/all.min.css" />
-        <link rel="preconnect" href="https://fonts.googleapis.com">
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-        <link href="https://fonts.googleapis.com/css2?family=Carlito:ital,wght@0,400;0,700;1,400;1,700&family=Raleway:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@100;300;400;500;700;900&display=swap" rel="stylesheet" />
       </Head>
 
-  
-      {children}
      
+      {children}
+   
     </div>
   )
 }
-
-
 
