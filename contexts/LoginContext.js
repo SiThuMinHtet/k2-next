471a7d3736240e32d4bd75644b12036d582754b7
yy-mizu <lynnx101@icloud.com>
2024-12-16 09:28:03 +0630

    lynn4

--- contexts/LoginContext.js
+++ contexts/LoginContext.js
@@ -5,29 +5,13 @@ const LoginContext = createContext()
 export function LoginProvider({ children }) {
   const [isAuthenticated, setIsAuthenticated] = useState(false)
 
-  const login = async (email, password) => {
-    // Implement login logic here
-    console.log('Logging in with:', email, password)
-    setIsAuthenticated(true)
-  }
 
-  const logout = () => {
-    // Implement logout logic here
-    setIsAuthenticated(false)
-  }
 
   return (
-    <LoginContext.Provider value={{ isAuthenticated, login, logout }}>
+    <LoginContext.Provider value={{ isAuthenticated }}>
       {children}
     </LoginContext.Provider>
   )
 }
 
-export const useLogin = () => {
-  const context = useContext(LoginContext)
-  if (context === undefined) {
-    throw new Error('useLogin must be used within a LoginProvider')
-  }
-  return context
-}
 
